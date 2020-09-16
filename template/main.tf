54a17924b315ea7d302902a24e3e0dccb778d171
Tushar Gohil <tushar.gohil@scet.ac.in>
2020-09-16 09:56:02 +0530

    g

--- template/main.tf
+++ template/main.tf
@@ -23,22 +23,22 @@ module "bigbluebutton_instance" {
   bbb_spot_price      = var.bbb_spot_price
 }
 
-module "scalelite_instance" {
-  source = "../modules/scalelite"
+# module "scalelite_instance" {
+#   source = "../modules/scalelite"
 
-  key_name            = var.key_name
-  security_group_name = var.scalelite_security_group_name
-  instance_type       = var.scalelite_instance_type
-  aws_ami             = var.scalelite_aws_ami
-  availability_zone   = var.availability_zone
-  scale_spot_price    = var.scale_spot_price
-  domain_name         = var.scalelite_domain_name
-  subdomain_name      = var.scalelite_subdomain_name
-  bbb_domain_name     = var.bigbluebutton_domain_name
-  bbb_subdomain_name  = var.bigbluebutton_subdomain_name
-  volume_size         = var.scalelite_volume_size
-  secret_key_base     = file("../secret_key_base")
-  scalelite_secret    = file("../scalelite_secret_key")
-  scalelite_url       = var.scalelite_url
-  nginx_ssl           = var.nginx_ssl
-}
+#   key_name            = var.key_name
+#   security_group_name = var.scalelite_security_group_name
+#   instance_type       = var.scalelite_instance_type
+#   aws_ami             = var.scalelite_aws_ami
+#   availability_zone   = var.availability_zone
+#   scale_spot_price    = var.scale_spot_price
+#   domain_name         = var.scalelite_domain_name
+#   subdomain_name      = var.scalelite_subdomain_name
+#   bbb_domain_name     = var.bigbluebutton_domain_name
+#   bbb_subdomain_name  = var.bigbluebutton_subdomain_name
+#   volume_size         = var.scalelite_volume_size
+#   secret_key_base     = file("../secret_key_base")
+#   scalelite_secret    = file("../scalelite_secret_key")
+#   scalelite_url       = var.scalelite_url
+#   nginx_ssl           = var.nginx_ssl
+# }
